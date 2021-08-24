333a089a9ce6f168e7fc1aeb0a6221d74b215487
Plethori <plethori10@gmail.com>
2021-08-24 23:47:14 +0800

    remove files

--- test/sample-test.js
+++
@@ -1,8 +0,0 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
-
-describe("PlethoriVault", function () {
-  it("Should return the new greeting once it's changed", async function () {
-  
-  });
-});
